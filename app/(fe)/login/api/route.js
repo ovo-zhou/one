addee11df52096f33cec065043ca40a7847aa2e9
ryan <ryanhhuang@tencent.com>
2024-08-03 15:46:28 +0800

    feat: 博客上线

--- app/(fe)/login/api/route.js
+++ app/(fe)/login/api/route.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken"
+import prisma from "@/prisma"
 
 export async function POST(request){
   const user=await request.json()
   const userfromDb=await prisma.user.findMany()
   const loginConfig={
-    username:userfromDb.length?userfromDb[0].username:process.env.username,
-    password:userfromDb.length?userfromDb[0].password:process.env.password
+    username:userfromDb.length?userfromDb[0].username:process.env.defaultUsername,
+    password:userfromDb.length?userfromDb[0].password:process.env.defaultPassword
   }
-  if(user.username===loginConfig.username&&user.password===loginConfig.password){
+  if(user.username===loginConfig.defaultUsername&&user.password===loginConfig.defaultPassword){
     const token=jwt.sign({username:user.username},process.env.JWT_SECRET,{expiresIn: '1h'})
     return new Response(JSON.stringify({
       code:0,
--- prisma/index.js
+++ prisma/index.js
@@ -1,10 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-let prisma = null
-if(global.prisma){
-  prisma=global.prisma
-}else{
-  prisma=new PrismaClient();
-  global.prisma=prisma
-}
+
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
