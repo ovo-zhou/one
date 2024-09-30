3e99c2eafd013120f8c0b2a9c751ea88bbd61919
ryan <ryanhhuang@tencent.com>
2024-09-30 14:37:57 +0800

    feat: fix tailwindcss base

--- app/components/link/index.jsx
+++
@@ -1,6 +0,0 @@
-import NextLink  from "next/link";
-import styles from './index.module.css'
-export default function Link(props){
-  const {href}=props;
-  return <NextLink className={styles.componentLink} href={href}>{props.children}</NextLink>
-}
