622f306363d01b5594dc40fc3ad4a702dba34380
ryan <ryanhhuang@tencent.com>
2025-06-23 11:04:02 +0800

    feat: 添加ts客户端

--- packages/client-ui/src/router/index.jsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from '../pages/home';
-export default function RouterMap(){
-  return <Router>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
-  </Router>
-}
