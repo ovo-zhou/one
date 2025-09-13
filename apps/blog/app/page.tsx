4705c35bf19dc3a7c9fb4775548f93f9f5f692df
ryan <ryanhhuang@tencent.com>
2025-09-13 18:46:56 +0800

    feat: 修复错误

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -5,19 +5,26 @@ const app = tcb.init({
   secretKey: process.env.SecretKey,
   env: "ryan-8gy6kj7jafc56d01",
 });
-const { models }=app;
+const { models } = app;
+const list = async () => {
+  try {
+    const { data } = await models.blog.list({
+      filter: {
+        where: {},
+      },
+      pageSize: 10, // 分页大小，建议指定，如需设置为其它值，需要和 pageNumber 配合使用，两者同时指定才会生效
+      pageNumber: 1, // 第几页
+      getCount: true, // 开启用来获取总数
+    });
+    return data;
+  } catch {
+    return { records: [] };
+  }
+};
 
 export default async function Home() {
-   const { data } = await models.blog.list({
-     filter: {
-       where: {},
-     },
-     pageSize: 10, // 分页大小，建议指定，如需设置为其它值，需要和 pageNumber 配合使用，两者同时指定才会生效
-     pageNumber: 1, // 第几页
-     getCount: true, // 开启用来获取总数
-     envType: "prod",
-   });  
-   const { records=[] } = data;
+  const data = await list();
+  const { records = [] } = data;
   return (
     <>
       <main>
@@ -27,9 +34,7 @@ export default async function Home() {
           </div>
         ))}
       </main>
-      <footer>
-        footer
-      </footer>
+      <footer>footer</footer>
     </>
   );
 }
