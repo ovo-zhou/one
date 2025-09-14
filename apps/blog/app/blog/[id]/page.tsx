6aa5fa2f68460cde1d9aa69848ba66d74cd8cf99
ryan <ryanhhuang@tencent.com>
2025-09-14 17:51:18 +0800

    feat(blog): 确保构建成功

--- apps/blog/app/blog/[id]/page.tsx
+++ apps/blog/app/blog/[id]/page.tsx
@@ -3,41 +3,53 @@ import dayjs from "dayjs";
 export const revalidate = 3600;
 const pageSize = 200;
 export async function generateStaticParams() {
-    const { data } = await models.blog.list({
-      filter: {
-        where: {},
-      },
-      pageSize, // 分页大小，建议指定，如需设置为其它值，需要和 pageNumber 配合使用，两者同时指定才会生效
-      pageNumber: 1, // 第几页
-      getCount: true, // 开启用来获取总数
-    });
-    const { records = [] } = data;
+  try{
+      const { data } = await models.blog.list({
+    filter: {
+      where: {},
+    },
+    pageSize, // 分页大小，建议指定，如需设置为其它值，需要和 pageNumber 配合使用，两者同时指定才会生效
+    pageNumber: 1, // 第几页
+    getCount: true, // 开启用来获取总数
+  });
+  const { records = [] } = data;
   return records.map((post) => ({
     id: String(post._id),
   }));
-}
 
+  }catch(_){
+    return []
+  }
+}
+const getBlog = async (id: string) => {
+  try {
+    const { data } = await models.blog.get({
+      filter: {
+        where: {
+          $and: [
+            {
+              _id: {
+                $eq: id, // 推荐传入_id数据标识进行操作
+              },
+            },
+          ],
+        },
+      },
+      // envType: pre 体验环境， prod 正式环境
+      envType: "prod",
+    });
+    return data;
+  } catch {
+    return {};
+  }
+};
 export default async function Page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-const { data } = await models.blog.get({
-  filter: {
-    where: {
-      $and: [
-        {
-          _id: {
-            $eq: id, // 推荐传入_id数据标识进行操作
-          },
-        },
-      ],
-    },
-  },
-  // envType: pre 体验环境， prod 正式环境
-  envType: "prod",
-});
+  const data = await getBlog(id);
   return (
     <main className="container">
       <h1 className="text-3xl font-bold">{data.title}</h1>
