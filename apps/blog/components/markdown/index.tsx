b5dcf71d5860f5aec0d7392f034b0c21883ed728
ryan <ryanhhuang@tencent.com>
2025-12-11 17:05:04 +0800

    feat(blog): 修复水合错误

--- apps/blog/components/markdown/index.tsx
+++ apps/blog/components/markdown/index.tsx
@@ -1,18 +1,18 @@
 'use client';
-import ReactMarkdown from "react-markdown";
-import components from "./componentMap";
-import { useEffect } from "react";
-import remarkGfm from "remark-gfm";
+import ReactMarkdown from 'react-markdown';
+import components from './componentMap';
+import { useEffect } from 'react';
+import remarkGfm from 'remark-gfm';
 export default function Markdown({ children }: { children: string }) {
-  const hash = window.location.hash.slice(1);
   useEffect(() => {
+    const hash = window.location.hash.slice(1);
     if (hash) {
       document.getElementById(hash)?.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
+        behavior: 'smooth',
+        block: 'start',
       });
     }
-  }, [hash]);
+  }, []);
   return (
     <div className="markdown">
       <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
